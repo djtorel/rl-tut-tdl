94ce7d05081e10dfb6e4ca23810a60487ba062a3
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-14 19:09:24 -0700

    added create_room function

--- src/map_utils.py
+++ src/map_utils.py
@@ -1,13 +1,25 @@
+class Rect():
+    """Holds dimensions"""
+
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+
+def create_room(game_map, room):
+    """Go through the tiles in the rectangle and make them passable."""
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            game_map.walkable[x, y] = True
+            game_map.transparent[x, y] = True
+
+
 def make_map(game_map):
-    """Create the map we want"""
+    """Create two rooms for demonstration purposes"""
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(35, 15, 10, 15)
 
-    for x, y in game_map:
-        game_map.walkable[x, y] = True
-        game_map.transparent[x, y] = True
-
-    game_map.walkable[30, 22] = False
-    game_map.transparent[30, 22] = False
-    game_map.walkable[31, 22] = False
-    game_map.transparent[31, 22] = False
-    game_map.walkable[32, 22] = False
-    game_map.transparent[32, 22] = False
+    create_room(game_map, room1)
+    create_room(game_map, room2)
