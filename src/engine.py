5e0f29511cca47b3f5b722925d163d7d5383aee1
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-14 17:27:56 -0700

    can move around, created event_handlers function

--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,6 @@
-import sys
-
 import tdl
+
+from input_handlers import handle_keys
 
 
 def main():
@@ -8,16 +8,21 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     tdl.set_font("font/arial12x12.png", greyscale=True, altLayout=True)
 
     root_console = tdl.init(screen_width, screen_height,
                             title='Roguelike Tutorial')
+    con = tdl.Console(screen_width, screen_height)
 
     while not tdl.event.is_window_closed():
-        root_console.draw_char(40, 25, '@', bg=None, fg=(255, 255, 255))
+        con.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
+        root_console.blit(con, 0, 0, screen_width, screen_height, 0, 0)
         tdl.flush()
 
-        root_console.draw_char(1, 1, ' ', bg=None)
+        con.draw_char(player_x, player_y, ' ', bg=None)
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
@@ -29,8 +34,22 @@ def main():
         if not user_input:
             continue
 
-        if user_input.key == 'ESCAPE':
-            sys.exit()
+        action = handle_keys(user_input)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
+            return True
+
+        if fullscreen:
+            tdl.set_fullscreen(not tdl.get_fullscreen())
 
 
 if __name__ == '__main__':
