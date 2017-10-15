2e7288d2ed2174c2338fcbffda8bfb9ff6c10eb1
Dominic Torruellas <dtorruellas@gmail.com>
2017-10-14 20:10:07 -0700

    randomly generated dungeon in - part 3 done

--- src/map_utils.py
+++ src/map_utils.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Rect():
     """Holds dimensions"""
 
@@ -6,6 +9,17 @@ class Rect():
         self.y1 = y
         self.x2 = x + w
         self.y2 = y + h
+
+    def center(self):
+        """Find center coordinates"""
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        """Returns true if this rectangle intersects with another one"""
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
 
 
 def create_room(game_map, room):
@@ -16,10 +30,70 @@ def create_room(game_map, room):
             game_map.transparent[x, y] = True
 
 
-def make_map(game_map):
-    """Create two rooms for demonstration purposes"""
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(35, 15, 10, 15)
+def create_h_tunnel(game_map, x1, x2, y):
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        game_map.walkable[x, y] = True
+        game_map.transparent[x, y] = True
 
-    create_room(game_map, room1)
-    create_room(game_map, room2)
+
+def create_v_tunnel(game_map, y1, y2, x):
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        game_map.walkable[x, y] = True
+        game_map.transparent[x, y] = True
+
+
+def make_map(game_map, max_rooms, room_min_size, room_max_size,
+             map_width, map_height, player):
+    """Create dungeon"""
+    rooms = []
+    num_rooms = 0
+    for r in range(max_rooms):
+        # Random width and height
+        w = randint(room_min_size, room_max_size)
+        h = randint(room_min_size, room_max_size)
+        # Random position without going out of the boundaries of the map
+        x = randint(0, map_width - w - 1)
+        y = randint(0, map_height - h - 1)
+
+        # "Rect" class makes rectangles easier to work with
+        new_room = Rect(x, y, w, h)
+
+        # Run through the other rooms to see if they intersect with
+        # this one
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                break
+        else:
+            # This means there are no intersections, so this room is
+            # valid
+
+            # "Paint" it to the map's tiles
+            create_room(game_map, new_room)
+
+            # Center coordinates of new room, will be useful later
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                # This is the first room, where player starts at
+                player.x = new_x
+                player.y = new_y
+            else:
+                # All rooms after the first:
+                # Connect it to the previous room with a tunnel
+
+                # Center coordinates of previous room
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                # Flip a coin (random number that is either 0 or 1)
+                if randint(0, 1) == 1:
+                    # First move horizontally, then vertically
+                    create_h_tunnel(game_map, prev_x, new_x, prev_y)
+                    create_v_tunnel(game_map, prev_y, new_y, new_x)
+                else:
+                    # First move virtically, then horizontally
+                    create_v_tunnel(game_map, prev_y, new_y, prev_x)
+                    create_h_tunnel(game_map, prev_x, new_x, new_y)
+
+            # Finally, append the new room to the list
+            rooms.append(new_room)
+            num_rooms += 1
